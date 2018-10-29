79774208cc6e665278e031fb6c7318cc4d94e4fe
Craig Walsh <crichardwalsh@gmail.com>
2018-10-28 18:54:35 -0600

    Commit after finishing jobs screen and functionality

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date_time = models.DateTimeField()
+    blog_body = models.TextField()
+    image = models.ImageField(upload_to='blog_images/')
+
